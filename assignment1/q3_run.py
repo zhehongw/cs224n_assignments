be9b057d5ab2dc4fdd23851aa036b4ff77378925
zhehongw <zhehongw@umich.edu>
2018-11-08 21:35:22 -0500

    q3 run

--- assignment1/q3_run.py
+++ assignment1/q3_run.py
@@ -36,6 +36,10 @@ wordVectors = sgd(
     lambda vec: word2vec_sgd_wrapper(skipgram, tokens, vec, dataset, C,
         negSamplingCostAndGradient),
     wordVectors, 0.3, 40000, None, True, PRINT_EVERY=10)
+#wordVectors = sgd(
+#    lambda vec: word2vec_sgd_wrapper(cbow, tokens, vec, dataset, C,
+#        negSamplingCostAndGradient),
+#    wordVectors, 0.3, 40000, None, True, PRINT_EVERY=10)
 # Note that normalization is not called here. This is not a bug,
 # normalizing during training loses the notion of length.
 
